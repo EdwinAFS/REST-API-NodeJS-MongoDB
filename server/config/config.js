b5045e1b7f1f9766a6477e62051a42643c8fc3dd
Edwin Fajardo <alexander_suarez1997@hotmail.com>
2020-05-02 17:16:27 -0500

    modificacion de la url de mongo

--- server/config/config.js
+++ server/config/config.js
@@ -7,7 +7,7 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev'){
 	urlDB = "mongodb://localhost/cafe"
 }else{
-	urlDB = "mongodb+srv://Edwin_AFS:12345Az!@cafedb-b5hvn.mongodb.net/test?retryWrites=true&w=majority"
+	urlDB = "mongodb+srv://Edwin_AFS:12345Az!@cafedb-b5hvn.mongodb.net/cafe?retryWrites=true&w=majority"
 }
 
 process.env.urlDB = urlDB;
